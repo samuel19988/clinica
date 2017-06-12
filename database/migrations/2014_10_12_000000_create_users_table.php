b8c53091fef652acb6b8432dfba6a975c53d7cd1
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2017-06-11 23:52:12 -0400

    demo da app con login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -15,12 +16,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre');
-            $table->string('apellido');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('rut')->unique();
-            $table->string('rol');
-            $table->date('especialidad');
+            $table->string('rut')->default("");
+            $table->string('rol')->default("");
+            $table->string('especialidad')->default("");
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => "Sofia Munoz",
+            'email' => 'sofimunoz97@outlook.com',
+            'password' => bcrypt('123456'),
+            'rut' => '1-9',
+            'rol' => 'medico',
+            'especialidad' => 'Oncologia'
+        ]);
+        User::create([
+            'name' => "Samuel Munoz",
+            'email' => 'samuel.munoz.f@gmail.com',
+            'password' => bcrypt('123456'),
+            'rut' => '2-7',
+            'rol' => 'medico',
+            'especialidad' => 'Urologo premium'
+        ]);
+        User::create([
+            'name' => "Leonardo Hidalgo",
+            'email' => 'lhidalgosep@gmail.com',
+            'password' => bcrypt('123456'),
+            'rut' => '3-6',
+            'rol' => 'medico',
+            'especialidad' => 'hola'
+        ]);
+    }
+}
