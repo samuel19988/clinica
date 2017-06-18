2baced03646892a48e16259d6b2ef30e1dbe3300
samuel19988 <samuel.munoz.f@gmail.com>
2017-06-18 12:39:23 -0400

    tablas

--- app/patient.php
+++ app/patient.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\exam;
 
 class patient extends Model
 {
-    //
+    public function exams(){
+        return $this->hasMany('App\exam')->get();
+    }
 }
