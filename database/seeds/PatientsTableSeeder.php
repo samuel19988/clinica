bbdc3a5f3bdc477ab43a0ceef96cec514b77e7e2
sofimunoze97 <sofimunoz97@outlook.com>
2017-06-19 18:04:44 -0400

    Creacion seed examen corregido y patient corregido

---
+++ database/seeds/PatientsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+Use \App\patient;
+use Faker\Factory as Faker;
+class PatientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 500; $i++) {
+            Patient::create([
+                'nombre' => $faker->name,
+                'apellido' => $faker->lastName,
+                'rut' => rand(300, 123456),
+                'fecha' => $faker->date(),
+                'email' => $faker->email,
+                'telefono' => $faker->phoneNumber
+            ]);
+        }
+    }
+}
