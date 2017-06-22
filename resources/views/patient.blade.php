f298366afdea788987d4dfa53a246d1384abb131
samuel19988 <samuel.munoz.f@gmail.com>
2017-06-21 21:07:05 -0400

    p

--- resources/views/patient.blade.php
+++ resources/views/patient.blade.php
@@ -3,19 +3,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-3 col-md-2 sidebar">
-                <ul class="nav nav-sidebar">
-                    <li class="active"><a href="#">Doctores: <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#"><i class="fa fa-calendar-o" aria-hidden="true"></i>Samuel Muñoz</a></li>
 
-                    <li><a href="#"><i class="fa fa-calendar-o" aria-hidden="true"></i>Sofia Muñoz</a></li>
-
-                    <li><a href="#"><i class="fa fa-calendar-o" aria-hidden="true"></i>Sebastian Montoya</a></li>
-
-                    <li><a href="#"><i class="fa fa-calendar-o" aria-hidden="true"></i>Josefina Vitulich</a></li>
-                </ul>
-
-            </div>
             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                 <div id='calendar' style="background-color: white"></div>
 
